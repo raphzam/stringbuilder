a696afeb15ec873a8dfeafb5b8de973fb13a0b5f
raphzam <raphaeljasonzamora@gmail.com>
2021-01-14 15:35:50 -0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args){
+
+        StringBuilder sb = new StringBuilder("hello world");
+
+        System.out.println(sb);
+
+        System.out.println(sb.reverse());
+
+        sb.append(", these are great");
+
+        System.out.println(sb);
+
+    }
+}
